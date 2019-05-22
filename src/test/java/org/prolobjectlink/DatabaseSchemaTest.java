e49113f7c1e4e13e0d9425179e8ec42a654f1bc2
Jose Zalacain <Jose Zalacain@AVALON>
2019-05-22 17:09:01 -0400

    Model Generation OK

--- src/test/java/org/prolobjectlink/DatabaseSchemaTest.java
+++ src/test/java/org/prolobjectlink/DatabaseSchemaTest.java
@@ -7,9 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.prolobjectlink.GraphEdge;
 import org.prolobjectlink.db.DatabaseClass;
 import org.prolobjectlink.db.DatabaseFunction;
 import org.prolobjectlink.db.DatabaseSequence;
@@ -29,7 +27,6 @@ public class DatabaseSchemaTest extends BaseTest {
 	}
 
 	@Test
-	@Ignore
 	public void testCompile() {
 		assertEquals(4, hschema.compile().size());
 		assertEquals(4, rschema.compile().size());
