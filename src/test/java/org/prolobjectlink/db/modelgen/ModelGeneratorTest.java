d9b41d2ac3e6fdfa4ef0b9a4dbdcac9efb2e1ab8
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-07 13:15:38 -0400

    Introduce model generation

--- src/test/java/org/prolobjectlink/db/modelgen/ModelGeneratorTest.java
+++ src/test/java/org/prolobjectlink/db/modelgen/ModelGeneratorTest.java
@@ -1,27 +1,7 @@
-/*-
- * #%L
- * prolobjectlink-jpx-jpl-yap
- * %%
- * Copyright (C) 2012 - 2019 Prolobjectlink Project
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 package org.prolobjectlink.db.modelgen;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
 
 import java.util.Collection;
 import java.util.Date;
@@ -29,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.prolobjectlink.BaseTest;
 import org.prolobjectlink.db.DatabaseClass;
@@ -102,12 +81,6 @@ public class ModelGeneratorTest extends BaseTest {
 		assertEquals(4, rg.createSchema().countClasses());
 		System.out.println(rg.compileSchema());
 
-	}
-
-	@Test
-	@Ignore
-	public void testWritePersistence() {
-		fail("Not Implemented yet"); // TODO
 	}
 
 }
