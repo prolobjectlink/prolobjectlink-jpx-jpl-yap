c23dc4ca5db0d8ee1d3cfec6d458650b6cbb311c
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-09-28 11:45:01 -0400

    Linux Scripts

--- bin/pllink.sh
+++ bin/pllink.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo $JAVA_HOME
+sudo touch /etc/ld.so.conf.d/java.conf
+echo $JAVA_HOME/jre/lib/amd64 | sudo tee -a /etc/ld.so.conf.d/java.conf
+echo $JAVA_HOME/jre/lib/amd64/server | sudo tee -a /etc/ld.so.conf.d/java.conf
+sudo ldconfig
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/jre/lib/rt.jar
+export SWI_HOME=/opt/pas-linux-x64-1.0.0/swi
+export PATH=$PATH:$SWI_HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pas-linux-x64-1.0.0/swi/packages/jpl
+export CLASSPATH=$CLASSPATH:/opt/pas-linux-x64-1.0.0/swi/packages/jpl/jpl.jar
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jpl.yap.YapPrologDatabaseConsole ${1+"$@"}
--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo $JAVA_HOME
+sudo touch /etc/ld.so.conf.d/java.conf
+echo $JAVA_HOME/jre/lib/amd64 | sudo tee -a /etc/ld.so.conf.d/java.conf
+echo $JAVA_HOME/jre/lib/amd64/server | sudo tee -a /etc/ld.so.conf.d/java.conf
+sudo ldconfig
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/jre/lib/rt.jar
+export SWI_HOME=/opt/pas-linux-x64-1.0.0/swi
+export PATH=$PATH:$SWI_HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pas-linux-x64-1.0.0/swi/packages/jpl
+export CLASSPATH=$CLASSPATH:/opt/pas-linux-x64-1.0.0/swi/packages/jpl/jpl.jar
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jpl.yap.YapPrologDatabaseConsole -m
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jpl.yap.YapPrologDatabaseConsole -j "$(dirname "$(pwd)")/lib/prolobjectlink-jpx-model.jar"
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jpl.yap.YapPrologDatabaseConsole -z 9110
