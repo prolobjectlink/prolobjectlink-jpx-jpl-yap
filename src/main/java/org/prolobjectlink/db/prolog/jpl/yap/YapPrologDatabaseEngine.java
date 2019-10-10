9806b0049e2518c5f7bccd3a00d3be7c5cfbd75d
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-10-10 16:13:20 -0400

    Introduce Object Unification

--- src/main/java/org/prolobjectlink/db/prolog/jpl/yap/YapPrologDatabaseEngine.java
+++ src/main/java/org/prolobjectlink/db/prolog/jpl/yap/YapPrologDatabaseEngine.java
@@ -19,27 +19,66 @@
  */
 package org.prolobjectlink.db.prolog.jpl.yap;
 
+import org.prolobjectlink.db.ObjectConverter;
 import org.prolobjectlink.db.prolog.PrologDatabaseEngine;
+import org.prolobjectlink.db.prolog.PrologObjectConverter;
 import org.prolobjectlink.db.prolog.PrologProgrammer;
 import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.PrologTerm;
 import org.prolobjectlink.prolog.jpl.yap.YapPrologEngine;
 
 public class YapPrologDatabaseEngine extends YapPrologEngine implements PrologDatabaseEngine {
 
+	private final ObjectConverter<PrologTerm> converter;
+
 	YapPrologDatabaseEngine() {
 		super(new YapPrologDatabaseProvider());
+		converter = new PrologObjectConverter(provider);
 	}
 
 	YapPrologDatabaseEngine(PrologProvider provider) {
 		super(provider);
+		converter = new PrologObjectConverter(provider);
 	}
 
 	YapPrologDatabaseEngine(PrologProvider provider, String file) {
 		super(provider, file);
+		converter = new PrologObjectConverter(provider);
+	}
+
+	public boolean unify(Object x, Object y) {
+		PrologTerm xt = converter.toTerm(x);
+		PrologTerm yt = converter.toTerm(y);
+		return unify(xt, yt);
 	}
 
 	public PrologProgrammer getProgrammer() {
 		return new YapPrologProgrammer(getProvider());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((converter == null) ? 0 : converter.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		YapPrologDatabaseEngine other = (YapPrologDatabaseEngine) obj;
+		if (converter == null) {
+			if (other.converter != null)
+				return false;
+		} else if (!converter.equals(other.converter))
+			return false;
+		return true;
 	}
 
 }
